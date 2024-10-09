2cbb6efe0c7f17819c53a8eb9a699f8e9af42694
AFURO-1130 <abcshotaro616@gmail.com>
2024-10-09 21:16:10 +0900

    fix

--- infra/main.tf
+++ infra/main.tf
@@ -40,6 +40,7 @@ resource "google_cloud_run_service" "backend" {
     percent         = 100
     latest_revision = true
   }
+  autogenerate_revision_name = true
 
   depends_on = [google_project_service.run_api]
 }
@@ -69,6 +70,7 @@ resource "google_cloud_run_service" "frontend" {
     percent         = 100
     latest_revision = true
   }
+  autogenerate_revision_name = true
 
   depends_on = [google_project_service.run_api]
 }
